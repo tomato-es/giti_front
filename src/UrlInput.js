4ead47b93d6f8bb939d7993fc06c9ed91172063f
thumbzzero <thumbzzero@gmail.com>
2022-12-03 01:56:40 +0900

    Initial input

---
+++ src/UrlInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UrlInput = () => {
+	return (
+		<div>
+			<img src='./image/github.jpg' alt='Github' />
+			<input type='url' />
+		</div>
+	);
+};
+
+export default UrlInput;
